7c1c975e1392352d9be16fc00216efb7050ed7b7
Sebastian <sebastian.agudelo@itac.co>
2024-12-16 20:54:41 -0500

    creacion del objeto User

---
+++ model/User.js
@@ -0,0 +1,14 @@
+export class Usuario {
+    constructor(documento, correo, contraseña, nombre, apellido, direccion, telefono) {
+      this.documento = documento;
+      this.correo = correo;
+      this.contraseña = contraseña;
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.direccion = direccion;
+      this.telefono = telefono;
+    }
+  
+    
+  }
+  
