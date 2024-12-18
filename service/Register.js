d794eafd3ea6ce8dc05590d8bb6079e602421412
Ana María <anicu2314@gmail.com>
2024-12-18 16:24:12 -0500

    Captura de datos desde el html de registro  al servicio Register

--- service/Register.js
+++ service/Register.js
@@ -6,16 +6,6 @@ import { TipoCuenta } from "../model/enums/TipoCuenta";
 let listaUsers = [];
 let cuentas= [];
 
-const documento = prompt("Ingrese su id:");
-const correoIngresado = prompt("Ingrese su correo:");
-const passwordIngresada = prompt("Ingrese su contraseña:");
-const nombreIngresado = prompt("Ingrese su nombre:");
-const apellidoIngresado = prompt("Ingrese su apellido:");
-const direccionIngresado = prompt("Ingrese su dirección:");
-const telefonoIngresado = prompt("Ingrese su teléfono:");
-
-const usuario1= new Usuario(documento, correoIngresado, passwordIngresada, nombreIngresado, apellidoIngresado, direccionIngresado, telefonoIngresado);
-
 function existeUsuario(correoIngresado){
     if(listaUsers.find((user) => user.correo === correoIngresado)===undefined){
         return false;
@@ -24,12 +14,27 @@ function existeUsuario(correoIngresado){
     }
 }
 
-if(!existeUsuario(correoIngresado)){
-    listaUsers.push(usuario1);
-    //Cuando el cliente se registra, se le asigna una cuenta de débito ¿?
-    const cuenta= new CuentaBancaria(usuario1.telefono, TipoCuenta.Debito, 0, usuario1.documento, EstadoCuenta.Activa);
-    cuentas.push(cuenta);
-    alert("Registro exitoso");
-}else{
-    alert("Registro fallido. Usuario ya existente");
-}
+const registroButton = document.getElementById("registro-button");
+registroButton.addEventListener("click", (event) => {
+
+    const documento = document.getElementById("documento").value;
+    const correoIngresado = document.getElementById("correo").value;
+    const passwordIngresada = document.getElementById("password").value;
+    const nombreIngresado = document.getElementById("nombre").value;
+    const apellidoIngresado = document.getElementById("apellido").value;
+    const telefonoIngresado = document.getElementById("telefono").value;
+
+    const usuario1= new Usuario(documento, correoIngresado, passwordIngresada, nombreIngresado, apellidoIngresado, telefonoIngresado);
+
+    if(!existeUsuario(correoIngresado)){
+        listaUsers.push(usuario1);
+        //Cuando el cliente se registra, se le asigna una cuenta de débito ¿?
+        //const cuenta= new CuentaBancaria(usuario1.telefono, TipoCuenta.Debito, 0, usuario1.documento, EstadoCuenta.Activa);
+        //cuentas.push(cuenta);
+        alert("Registro exitoso");
+    }else{
+        alert("Registro fallido. Usuario ya existente");
+    }
+
+});
+
