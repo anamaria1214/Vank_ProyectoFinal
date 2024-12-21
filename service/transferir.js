f98cb0744591f78c5aae38018a071a0ce9543c7d
Ana María <anicu2314@gmail.com>
2024-12-20 19:18:14 -0500

    Retiro y deposito de las cuentas a trasnferir

--- service/transferir.js
+++ service/transferir.js
@@ -1,19 +1,31 @@
 import { Transferencia } from "../model/transfer";  
 import { Usuario } from "../model/User";
+import { CuentaBancaria } from "../model/Account";
 
 let transferencias= []
+let cuentas=[]
 
+//Estos objetos se obtienen de la pagina
 const usuario= new Usuario();
+const cuenta= new CuentaBancaria();
 
 const transferir = document.getElementById("transferir");
 
+function obtenerCuenta(numCuenta) {
+    return cuentas.some((cuenta) => cuenta.numeroCuenta === numCuenta);
+}
+
 transferir.addEventListener("click", (event) => {
     const cuentaTransferir= document.getElementById("numeroCuenta").value;
+
+    const cuentaATransferir= obtenerCuenta(cuentaTransferir);
     const montoTransferir= document.getElementById("monto").value;
     const motivoTransferir= document.getElementById("motivo").value;
 
     const transferencia= new Transferencia(usuario.documento, cuentaTransferir, montoTransferir, motivoTransferir);
 
+    cuenta.retirar(monto);
+    cuentaATransferir.depositar(monto);
     transferencias.push(transferencia);
 
 });
